65b1364b3a14feccc4aad12b3d090d8b4948923b
EdmerFco <franciscohr.tij18@utsjr.edu.mx>
2022-05-08 11:33:54 -0500

    add charactrer class and character design

--- asset/js/app.js
+++ asset/js/app.js
@@ -1,1 +1,50 @@
-alert('Hello world');
+// import API from './api.js'
+
+// const api = new API()
+
+// const $CharacterContainer = document.querySelector('#character-container');
+// // const charaterTemplate = 
+// // `
+// // <article class="character">
+// //         <div class="character-grid">
+// //             <h2>Rick Sanchez</h2>
+// //             <img src="" alt="">
+// //         </div>
+// //     </article>
+// // `
+// class Charater{
+//     constructor({name, image}) {
+//         this.name = name
+//         this.image = image
+//         this.reder();
+//     }
+//     build(){
+//         return`
+//         <article class="character">
+//         <div class="character-grid">
+//             <h2>${this.name }</h2>
+//             <img src="${this.image}" alt="">
+//         </div>
+//     </article>
+//     `
+//     }
+//     reder() {
+//         $CharacterContainer.innerHTML = this.build();
+//     }
+// }
+ 
+// // const rick = new Charater({
+// //     name: 'Edmer',
+// //     img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRMLiOr1cDRQBjdWI2Udjho69d6MiKzEinWMw&usqp=CAU'
+// // })
+
+// async function initAPP(initCharacterId) {
+//     const characterData = await api.getCharacter(initCharacterId);
+//     console.log(characterData);
+    
+// const rick = new Charater(characterData)
+    
+// }
+// initAPP(1);
+// // rick.reder();
+// // console.log(api.getCharacter(1));
